927a3c80caf665292f223d2f6712b3445fb3ee92
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-04-27 19:55:43 +0200

    Episode 08

---
+++ Episode 08/Assets/Scripts/Chaser.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser : MonoBehaviour {
+
+	public Transform targetTransform;
+	public float speed = 0;
+
+	void Update () {
+		Vector3 displacementFromTarget = targetTransform.position - transform.position;
+		Vector3 directionToTarget = displacementFromTarget.normalized;
+		Vector3 velocity = directionToTarget * speed;
+
+		float distanceToTarget = displacementFromTarget.magnitude;
+
+		if (distanceToTarget > 1.5f) {
+			transform.Translate (velocity * Time.deltaTime);
+		}
+	
+	}
+}
---
+++ Episode 08/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10;
+
+	void Update () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 direction = input.normalized;
+		Vector3 velocity = direction * speed;
+		Vector3 moveAmount = velocity * Time.deltaTime;
+
+		//transform.position += moveAmount;
+		transform.Translate(moveAmount);
+	}
+}
