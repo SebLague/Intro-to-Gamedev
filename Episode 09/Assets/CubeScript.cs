2df9df50dd686a3cf4c915982b45da17af458174
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-05-21 10:41:31 +0200

    Episode 09

---
+++ Episode 09/Assets/CubeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	public Transform sphereTransform;
+
+	// Use this for initialization
+	void Start () {
+		sphereTransform.parent = transform;
+		sphereTransform.localScale = Vector3.one * 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * Time.deltaTime * 180, Space.World);
+		transform.Translate (Vector3.forward * Time.deltaTime * 7, Space.World);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			sphereTransform.localPosition = Vector3.zero;
+		}
+	}
+}
