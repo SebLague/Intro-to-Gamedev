215b311078ee22a8a2cefb51929ab915ce24534f
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-05-22 19:02:40 +0200

    Episode 10

---
+++ Episode 10/Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 6;
+	int coinCount;
+	Vector3 velocity;
+
+	Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 direction = input.normalized;
+		velocity = direction * speed;
+	}
+
+	void FixedUpdate() {
+		myRigidbody.position += velocity * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider triggerCollider) {
+		if (triggerCollider.tag == "Coin") {
+			Destroy (triggerCollider.gameObject);
+			coinCount++;
+		}
+	}
+
+}
