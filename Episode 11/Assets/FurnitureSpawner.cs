0d72860428bdfca616a1873c26cc4adeed4d0c0e
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-05-27 15:16:23 +0200

    Episode 11

---
+++ Episode 11/Assets/FurnitureSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnitureSpawner : MonoBehaviour {
+
+	public GameObject chairPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Vector3 randomSpawnPosition = new Vector3 (Random.Range (-10f, 10), 0, Random.Range (-10f, 10f));
+			Vector3 randomSpawnRotation = Vector3.up * Random.Range (0, 360);
+
+			GameObject newChair = (GameObject)Instantiate (chairPrefab, randomSpawnPosition, Quaternion.Euler (randomSpawnRotation));
+			newChair.transform.parent = transform;
+		}
+
+	}
+}
