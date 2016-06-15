0e5e0ed4088a843b97cdb09940c04842675919d4
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2016-06-15 11:16:56 +0200

    Episode 13

---
+++ Episode 13/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 7;
+
+	float screenHalfWidthInWorldUnits;
+
+	// Use this for initialization
+	void Start () {
+
+		float halfPlayerWidth = transform.localScale.x / 2f;
+		screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		float velocity = inputX * speed;
+		transform.Translate (Vector2.right * velocity * Time.deltaTime);
+
+		if (transform.position.x < -screenHalfWidthInWorldUnits) {
+			transform.position = new Vector2 (screenHalfWidthInWorldUnits, transform.position.y);
+		}
+
+		if (transform.position.x > screenHalfWidthInWorldUnits) {
+			transform.position = new Vector2 (-screenHalfWidthInWorldUnits, transform.position.y);
+		}
+
+	}
+}
